a3a77e2651f14fcf7567117ff92d42664e91af82
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-04-29 23:21:43 +0200

    Update R_BasicsQuiz.R

--- R_BasicsQuiz.R
+++ R_BasicsQuiz.R
@@ -11,3 +11,6 @@ random_vector <- c("R","is","great")
 some_vector <- c(25555,342343,123123123,4234234,53243234,54324234,5421111,12312312,111231,
                      1231231,12312312,12312312,123123,898972,789872,2343,23423423,2343221,23423,
                      14444,44324222,2342341,124231111,22233345,1111233333,1231231,1231231)
+
+
+# How many rows and columns does the congress dataframe have? Use a function to show its data type. You must use R code to generate these values.
