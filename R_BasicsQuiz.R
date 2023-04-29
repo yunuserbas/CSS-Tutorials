c9ecdc75e5be60d4dc638d73c29dce21767b1b43
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-04-29 23:20:46 +0200

    Update R_BasicsQuiz.R

--- R_BasicsQuiz.R
+++ R_BasicsQuiz.R
@@ -1,7 +1,13 @@
 
 # 1- Extract the third element of the vector using numerical indexing.
-# random_vector <- c("R","is","great")
+
+random_vector <- c("R","is","great")
 
 
 
 
+# 2- Use R code to identify the data type of some_vector. What is the largest number in this vector? How about the mean value?
+
+some_vector <- c(25555,342343,123123123,4234234,53243234,54324234,5421111,12312312,111231,
+                     1231231,12312312,12312312,123123,898972,789872,2343,23423423,2343221,23423,
+                     14444,44324222,2342341,124231111,22233345,1111233333,1231231,1231231)
