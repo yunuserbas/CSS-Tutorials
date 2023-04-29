4b8722b3112945a01a1acb5c12fb0f0145f6c940
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-04-29 23:22:31 +0200

    Update R_BasicsQuiz.R

--- R_BasicsQuiz.R
+++ R_BasicsQuiz.R
@@ -13,4 +13,10 @@ some_vector <- c(25555,342343,123123123,4234234,53243234,54324234,5421111,123123
                      14444,44324222,2342341,124231111,22233345,1111233333,1231231,1231231)
 
 
-# How many rows and columns does the congress dataframe have? Use a function to show its data type. You must use R code to generate these values.
+# 3- How many rows and columns does the congress dataframe have? Use a function to show its data type. You must use R code to generate these values.
+
+
+# 4- What is the average age of all congress members? What is the data type of the birthyear column?
+
+
+# 5- How much older is Sherrod Brown (a member of congress) compared to the average of members of congress? How about Dianne Feinstein?
