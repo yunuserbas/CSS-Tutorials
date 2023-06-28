b3e5cd2d610d1a8039490cefd5884d34522e0a51
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-06-28 22:29:49 +0200

    Update R_basics.R

---
+++ R/R_basics.R
@@ -0,0 +1,47 @@
+
+
+my_object <- (2/4)*5
+
+MyVector <- c(1, 2, 3)
+
+my_string_vector <- c("this", "is", "a", "vector")
+
+mean(MyVector)
+
+MyVector[3]
+
+load("~/Desktop/Coronavirus Tests.Rdata")
+
+setwd("~/Desktop")
+
+covid_tweets$tweet_text
+
+covid_tweets$tweet_text[3]
+
+# 1- Extract the third element of the vector using numerical indexing.
+
+random_vector <- c("R","is","great")
+
+
+
+
+# 2- Use R code to identify the data type of some_vector. What is the largest number in this vector? How about the mean value?
+
+some_vector <- c(25555,342343,123123123,4234234,53243234,54324234,5421111,12312312,111231,
+                     1231231,12312312,12312312,123123,898972,789872,2343,23423423,2343221,23423,
+                     14444,44324222,2342341,124231111,22233345,1111233333,1231231,1231231)
+
+
+# 3- How many rows and columns does the congress dataframe have? Use a function to show its data type. You must use R code to generate these values.
+
+
+# 4- What is the average age of all congress members? What is the data type of the birthyear column?
+
+
+# 5- How much older is Sherrod Brown (a member of congress) compared to the average of members of congress? How about Dianne Feinstein?
+
+
+# 6- Who are the oldest members of congress?
+
+
+
