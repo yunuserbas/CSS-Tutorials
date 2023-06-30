307b00359678c6a3620764d9b9b88b9369be7a39
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-06-30 17:33:25 +0200

    Update 2-DataWrangling.R

--- R/2-DataWrangling.R
+++ R/2-DataWrangling.R
@@ -19,3 +19,12 @@ pipe operator ("%>%"):
 # 4. Using mutate, create a new variable called age which represents the approximate age of each member of congress. How many democratic senators are over 60 years old?
 # Note: you can approximate age using the formula age = 2021-birthyear.
 
+# 5. Create a new column that indicates whether or not the member of congress is more than 55 years old, 
+# and create a single dataframe showing the number of male and female members of congress that are over and under 55.
+# Note: the dataframe should have four rows: number of females over 55, number of males over 55, number of females under 55, number of males under 55.
+
+# 6. Using gather, create a new dataframe where each row corresponds to a valid twitter, facebook, or youtube social media account, then compute the total number of accounts for each political party. 
+# Then do the same with pivot_longer.
+# Note: not every congress member has an account on all three platform, so be sure to filter.
+# Note: you may need to look up the documentation for pivot_longer.
+
