a52bd36ef8b1fe75c98aad4c88a87ff03c551458
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-06-30 17:31:52 +0200

    Update 2-DataWrangling.R

--- R/2-DataWrangling.R
+++ R/2-DataWrangling.R
@@ -13,3 +13,9 @@ pipe operator ("%>%"):
 
 # 2. Create a new dataframe that includes only senators and the columns gender, birthyear, and party. 
 # Then use that new dataframe to compute the number of male and female democrats and republicans (the output should be five rows corresponding to female democrats, male democrats, male independents, female republicans, and male republicans).
+
+# 3. Identify the oldest and youngest male and female democrat senators using tidyverse functions.
+
+# 4. Using mutate, create a new variable called age which represents the approximate age of each member of congress. How many democratic senators are over 60 years old?
+# Note: you can approximate age using the formula age = 2021-birthyear.
+
