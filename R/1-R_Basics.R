8327d7d2992a33073eabba1281bcd88423831245
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-07-04 21:37:36 +0200

    Update 1-R_Basics.R

--- R/1-R_Basics.R
+++ R/1-R_Basics.R
@@ -29,6 +29,17 @@ some_vector <- c(25555,342343,123123123,4234234,53243234,54324234,5421111,123123
                      1231231,12312312,12312312,123123,898972,789872,2343,23423423,2343221,23423,
                      14444,44324222,2342341,124231111,22233345,1111233333,1231231,1231231)
 
+data_type <- class(some_vector)
+print(data_type)
+
+
+largest_number <- max(some_vector)
+print(largest_number)
+
+
+mean_value <- mean(some_vector)
+print(mean_value)
+
 
 # 3- How many rows and columns does the congress dataframe have? Use a function to show its data type. You must use R code to generate these values.
 
