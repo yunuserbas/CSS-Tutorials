5b14c0a005282399718eb98d8ddc8ab21c48b8ec
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-07-06 23:15:54 +0200

    Update 2-DataWrangling.R

--- R/2-DataWrangling.R
+++ R/2-DataWrangling.R
@@ -3,13 +3,13 @@
 
 # 1. Describe what the following tidyverse functions do. Also describe the pipe operator “%>%”. You may need to look up the official documentation for each of these.
 
-filter: 
-select: 
-mutate: 
-count: 
-arrange: 
-gather: 
-pipe operator ("%>%"): 
+filter: selects rows
+select: selects columns
+mutate: allows you to change an existing variable or add a new variable
+count: gives you the number of each observations of a given variable_name in a dataframe
+arrange: sorts dataframe in alphabetical order by variable_name
+gather: transforms data into long structure (wide structure==lots of columns; long==lots of rows)
+pipe operator ("%>%"): channel data into a function
 
 # 2. Create a new dataframe that includes only senators and the columns gender, birthyear, and party. 
 # Then use that new dataframe to compute the number of male and female democrats and republicans (the output should be five rows corresponding to female democrats, male democrats, male independents, female republicans, and male republicans).
