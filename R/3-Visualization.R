2dd7fbee5518842ef618e193a6f2ad53cffa969f
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-07-17 22:43:45 +0200

    Update 3-Visualization.R

--- R/3-Visualization.R
+++ R/3-Visualization.R
@@ -1,1 +1,9 @@
+# Questions
 
+# 1. What arguments can you use for the mean function in R?
+
+Syntax: mean(x, trim, na.rm)
+
+# 2. Extract the third element of the vector using numerical indexing.
+
+random_vector <- c("R","is","great")
