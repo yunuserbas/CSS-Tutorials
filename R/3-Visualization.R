ae4ab32ad57f6f8d81476c681250511d157a760e
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-07-17 22:45:38 +0200

    Update 3-Visualization.R

--- R/3-Visualization.R
+++ R/3-Visualization.R
@@ -10,3 +10,9 @@ random_vector <- c("R","is","great")
 
 random_vector[3]
 
+# 3. Use R code to identify the data type of some_vector. What is the largest number in this vector? How about the mean value?
+
+some_vector <- c(25555,342343,123123123,4234234,53243234,54324234,5421111,12312312,111231,1231231,123123)
+
+typeof(some_vector)
+
