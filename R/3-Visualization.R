68042f8ea28b6a5973af4d1f92dcd3673f63795c
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-07-19 23:17:24 +0200

    Update 3-Visualization.R

--- R/3-Visualization.R
+++ R/3-Visualization.R
@@ -1,16 +1,26 @@
 # Questions
 
-# 1. What arguments can you use for the mean function in R?
+# 1. Create a bar plot to show the average ages of democrat and republican congress members. Now do the same for M and F genders (this second part should include members of all parties).
 
-Syntax: mean(x, trim, na.rm)
+library(ggplot2)
+plotdf <- df |> filter(party=='Democrat' | party=='Republican')
 
-# 2. Extract the third element of the vector using numerical indexing.
+p <- ggplot(data=plotdf, aes(x=party, y=age)) + geom_bar(stat = "summary", fun = "mean")
 
-random_vector <- c("R","is","great")
+p
 
-random_vector[3]
+# 2. Create two bar charts: one that shows the total number of social media accounts among democrats and republicans (Twitter, Facebook, YouTube), and one that shows the average number of accounts per-politician
+# for each party. Which political party has more social media accounts? Which party has a higher per-politician average?.
 
-# 3. Use R code to identify the data type of some_vector. What is the largest number in this vector? How about the mean value?
+# Note: there are several ways to accomplish this. You could use gather again and then group_by and summarise within politician and then within party, or you could use mutate to get counts for each politican
+# and then average by party. Any other approach is also fine.
+
+p <- ggplot(data=all3accounts_join, aes(x=party, y=n)) + geom_bar(stat = "identity")
+
+p 
+
+# 3. Use an inner join to combine the columns of the committees dataframe with the columns of congress, and create a plot showing the average number of committees that democrats and republicans belong to. Next
+# create a plot showing the averages by gender (note: this second part should include members of other parties as well).
 
 some_vector <- c(25555,342343,123123123,4234234,53243234,54324234,5421111,12312312,111231,1231231,123123)
 
@@ -37,3 +47,4 @@ avg_age
 typeof(congress$birthyear)
 
 
+# 6. 
