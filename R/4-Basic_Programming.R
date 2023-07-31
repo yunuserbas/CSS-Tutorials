fb0c9fde8928628240ab19d6a1418ac4ff90eda6
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-07-31 22:53:42 +0200

    Update 4-Basic_Programming.R

--- R/4-Basic_Programming.R
+++ R/4-Basic_Programming.R
@@ -1,1 +1,7 @@
 
+# 1. Packages in R can contain many useful functions/commands. If you didn’t know what a certain function did, or how it worked, 
+# where within RStudio would you look to learn more / see example code? Where would you look outside RStudio?
+
+Within RStudio: help() function 
+Outside RStudio: R documentation
+
