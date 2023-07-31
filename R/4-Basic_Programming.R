cdcbdbd6a557b1b2405e763040ab6d9cba7dc0b1
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-07-31 22:55:44 +0200

    Update 4-Basic_Programming.R

--- R/4-Basic_Programming.R
+++ R/4-Basic_Programming.R
@@ -5,3 +5,16 @@
 Within RStudio: help() function 
 Outside RStudio: R documentation
 
+# 2. Write a function that takes a character vector as an argument and returns a character vector containing the first letters of each element in the original vector.
+# To show that it works, test it on the character vector sentence defined below:
+
+sentence <- c('you', 'only', 'understand', 'data', 'if', 'data', 'is', 'tidy')
+
+first_char <- function(char_vector) { 
+output_char_vector <- c()
+for(word in char_vector){ output_char_vector <- append(output_char_vector, substr(word, 1, 1))
+}
+return (output_char_vector)
+}
+
+first_char(sentence)
