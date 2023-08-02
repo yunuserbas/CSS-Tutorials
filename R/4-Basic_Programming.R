e025c9171a9abd660e4779f272eb1215ee26ec14
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-08-02 19:58:25 +0200

    Update 4-Basic_Programming.R

--- R/4-Basic_Programming.R
+++ R/4-Basic_Programming.R
@@ -30,3 +30,6 @@ year_to_age <- function(date) {
 
 df <- congress |> mutate (age = year_to_age(birthdate))
 
+# 4. Create your own function which accepts a birthyear vector and returns an approximate current age, 
+# then use it on the birthyear column of the congress dataframe to create a new age column with mutate.
+
